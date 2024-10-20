60ae53bde7f91be6bdeb2c03c7a1a99aa70e32b0
eugenesvk <eugenesvk@users.noreply.github.com>
2024-10-21 00:58:07 +0700

    helper: update p macro use to propagate errors

--- parser/helper/src/helper.rs
+++ parser/helper/src/helper.rs
@@ -1,4 +1,9 @@
+use std::error::Error;
+use std::result;
+
 use crate::alias::*;
+
+type Result<T> = result::Result<T, Box<dyn Error>>;
 // pub allows use in other files
 pub fn type_of      <T>(_: T) -> &'static str {          type_name::<T>() }
-pub fn print_type_of<T>(_:&T)                 { p!("{}", type_name::<T>());}
+pub fn print_type_of<T>(_:&T) -> Result<()>  	{p!("{}", type_name::<T>())?; Ok(())}
