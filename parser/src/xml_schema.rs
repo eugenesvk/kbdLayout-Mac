e5af0d82aecc496dcb949344bfefff7433e695ca
eugenesvk <eugenesvk@users.noreply.github.com>
2024-10-21 17:31:23 +0700

    xml schema: remove serialize derive

--- parser/src/xml_schema.rs
+++ parser/src/xml_schema.rs
@@ -1,6 +1,6 @@
 // generated with the help of github.com/Thomblin/xml_schema_generator
 use serde::{Deserialize,Serialize};
-#[derive(Debug,Serialize,Deserialize)] pub struct Keyboard { //L0 <keyboard group="126" id="-7151" name="TypES" maxout="3">
+#[derive(Debug,Deserialize)] pub struct Keyboard { //L0 <keyboard group="126" id="-7151" name="TypES" maxout="3">
   #[serde(rename="@group"     	)] pub group       	: u16,
   #[serde(rename="@id"        	)] pub id          	: i32,
   #[serde(rename="@name"      	)] pub name        	: String,
@@ -12,66 +12,66 @@ use serde::{Deserialize,Serialize};
   pub                         	       actions     	: Actions,
   pub                         	       terminators 	: Terminators,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Layouts { //L1
+#[derive(Debug,Deserialize)] pub struct Layouts { //L1
   #[serde(rename="$text"	)] pub text  	: Option<String>,
   pub                   	       layout	: Vec<Layout>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Layout { //L2 <layout first="0" last="17" mapSet="16c" modifiers="f4"/>
+#[derive(Debug,Deserialize)] pub struct Layout { //L2 <layout first="0" last="17" mapSet="16c" modifiers="f4"/>
   #[serde(rename="@first"    	)] pub first    	: u16,
   #[serde(rename="@last"     	)] pub last     	: u16,
   #[serde(rename="@mapSet"   	)] pub map_set  	: String,
   #[serde(rename="@modifiers"	)] pub modifiers	: String,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct ModifierMap {
+#[derive(Debug,Deserialize)] pub struct ModifierMap {
   #[serde(rename="@id"          	)] pub id            	: String,
   #[serde(rename="@defaultIndex"	)] pub default_index 	: u16,
   #[serde(rename="$text"        	)] pub text          	: Option<String>,
   #[serde(rename="keyMapSelect" 	)] pub key_map_select	: Vec<KeyMapSelect>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct KeyMapSelect {
+#[derive(Debug,Deserialize)] pub struct KeyMapSelect {
   #[serde(rename="@mapIndex"	)] pub map_index	: u16,
   #[serde(rename="$text"    	)] pub text     	: Option<String>,
   pub                       	       modifier 	: Vec<Modifier>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Modifier {
+#[derive(Debug,Deserialize)] pub struct Modifier {
   #[serde(rename="@keys"	)] pub keys	: String,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct KeyMapSet {
+#[derive(Debug,Deserialize)] pub struct KeyMapSet {
   #[serde(rename="@id"   	)] pub id     	: String,
   #[serde(rename="$text" 	)] pub text   	: Option<String>,
   #[serde(rename="keyMap"	)] pub key_map	: Vec<KeyMap>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct KeyMap {
+#[derive(Debug,Deserialize)] pub struct KeyMap {
   #[serde(rename="@index"     	)] pub index       	: u16,
   #[serde(rename="@baseIndex" 	)] pub base_index  	: Option<String>,
   #[serde(rename="@baseMapSet"	)] pub base_map_set	: Option<String>,
   #[serde(rename="$text"      	)] pub text        	: Option<String>,
   pub                         	       key         	: Vec<Key>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Key {
+#[derive(Debug,Deserialize)] pub struct Key {
   #[serde(rename="@code"  	)] pub code  	: String,
   #[serde(rename="@action"	)] pub action	: Option<String>,
   #[serde(rename="@output"	)] pub output	: Option<String>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Actions {
+#[derive(Debug,Deserialize)] pub struct Actions {
   #[serde(rename="$text"	)] pub text  	: Option<String>,
   pub                   	       action	: Vec<Action>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Action {
+#[derive(Debug,Deserialize)] pub struct Action {
   #[serde(rename="@id"  	)] pub id  	: String,
   #[serde(rename="$text"	)] pub text	: Option<String>,
   pub                   	       when	: Vec<ActionWhen>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct ActionWhen {
+#[derive(Debug,Deserialize)] pub struct ActionWhen {
   #[serde(rename="@state" 	)] pub state 	: String,
   #[serde(rename="@next"  	)] pub next  	: Option<String>,
   #[serde(rename="@output"	)] pub output	: Option<String>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct Terminators {
+#[derive(Debug,Deserialize)] pub struct Terminators {
   #[serde(rename="$text"	)] pub text	: Option<String>,
   pub                   	       when	: Vec<TerminatorsWhen>,
 }
-#[derive(Debug,Serialize,Deserialize)] pub struct TerminatorsWhen {
+#[derive(Debug,Deserialize)] pub struct TerminatorsWhen {
   #[serde(rename="@state" 	)] pub state 	: String,
   #[serde(rename="@output"	)] pub output	: String,
 }
